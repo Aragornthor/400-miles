1a4caee1144d1ee5333d5ddbdc3375f4ea74fc13
Virgile Dassonneville <virgile@ThinkPad-T440.home>
2021-04-10 16:01:57 +0200

    taille carte

--- src/serveur.c
+++ src/serveur.c
@@ -66,6 +66,7 @@ void genererCartes(void) {
             pioche[i] = creerCarte("Accident", PIEGE, 0, "Impose un arrêt au joueur ciblé");
         } else {
             pioche[i] = creerCarte("Fin de limitation", AVANTAGE, 0, "Annule les ralentissements");
+            printf("tailel carte %d\n", sizeof(pioche[i]));
         }
     }
 }
