7219a1b667d516fbd002ce2266e178a03e75ffe9
Virgile Dassonneville <virgile@ThinkPad-T440.home>
2021-04-10 20:05:13 +0200

    LES TUBES

--- src/client.c
+++ src/client.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "string.h"
 
 
 #include "../dependencies/map.h"
@@ -16,8 +17,25 @@
 #define true 1
 #define false 0
 
+int writer_fifo;
+
+void init_writer(void) {
+    mkfifo("game.fifo", 0666);
+    writer_fifo = open("game.fifo", O_WRONLY);
+}
+
+
+
 int main(void) {
     printf("Bienvenue dans le client du 400 miles !");
+
+    init_writer();
+    char msg[256];
+    do {
+        fgets(msg, 256, stdin);
+        write(writer_fifo, msg, strlen(msg)+1);
+    } while (strcmp(msg, "STOP\n") != 0);
+    close(writer_fifo);
 
     return 0;
 }
