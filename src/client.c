54db186863410008cfdd251a67f8c2b62ee95330
Aragornthor <qtn.delmarre@gmail.com>
2021-04-10 19:58:52 +0200

    Création du client

---
+++ src/client.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#include "../dependencies/map.h"
+#include "../dependencies/joueur.h"
+#include "../dependencies/carte.h"
+
+#define true 1
+#define false 0
+
+int main(void) {
+    printf("Bienvenue dans le client du 400 miles !");
+
+    return 0;
+}
