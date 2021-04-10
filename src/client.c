b2ba47b640e0d94904d3e3d06f2ac2537afe34e4
Aragornthor <qtn.delmarre@gmail.com>
2021-04-10 20:01:01 +0200

    Affichage taille du terminal client

--- src/client.c
+++ src/client.c
@@ -17,7 +17,12 @@
 #define false 0
 
 int main(void) {
-    printf("Bienvenue dans le client du 400 miles !");
+    printf("Bienvenue dans le client du 400 miles !\n");
+
+    // On récupère la taille du terminal
+    struct winsize ws;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+    printf("Dimension du terminal = %dx%d\n", ws.ws_col, ws.ws_row); // Contrôle durant le dev, TODO retirer cette ligne
 
     return 0;
 }
